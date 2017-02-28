0e4cfe695f5acf5e4caf5f29aaeb983ed8afb789
James Opzoomer <james.opzoomer@gmail.com>
2017-02-28 14:20:47 +0000

    New scripts modified for functionality with multiple panels, also script modified to read a csv into an fas file.

--- RScript.SPADE.TSNE.R
+++ RScript.SPADE.TSNE.R
@@ -1,107 +1,201 @@
-# JO Initial rough script 30/01/17
+# JO working script with updates from Shahram
+# 15/02/2017
+
+# To Do list:
+# Can I split the populations by keyword 
+# Then add an pseudovalue column so that we can map the cellular idenitites back on the nodes
+
+
+
 # load the SPADE library
 library(spade)
+
+# load flowR functional libraries
+library(openCyto)
+library(ggcyto)
+
+# Load the heatmap functionality libraries
+# Note heat map will transition to Superheat  
 library(gplots)
 library(pheatmap)
+library(superheat)
 library(RColorBrewer)
 
-# To Do:
-# In theory we want to split the populations by keyword and add an pseudovalue column so that we can map the cellular idenitites back on the nodes
 
 # Use a file exported from FlowJo, must contain the t-SNE values within it
-# Should export compensated paramters
-data_file_path = '942_UNCOMP_TAM Ter119 perpc55 Cd45 FITC F480 BV410 MHC II PE CD11c 780 CD11b 510_942 cd206 A_CD45+.fcs'
+# Should export uncompensated parameters
 
-# QC
-fsc_file <- flowCore::read.FCS(data_file_path)
-fsc_file
 
-# Identify number if cell clusters, not 100% sure how many is optimal, but it appears that overclustring is important
+data_file_path = '1000_TAM Ter119 perpc55 Cd45 780 F480 BV410 MHC II PE Ly6C 488 CD11b 510_1007 CD206A_Single Cells.fcs'
+
+QC_fcs_file <- function(data_file_path) {
+  # QC with user interface to check
+  fsc_file <- flowCore::read.FCS(data_file_path)
+  fsc_file
+
+}
+
+# This function should make dotplots of the tSNE graphs
+# tSNE graphs should be heat cloloured by channel
+# With scale transform specified
+
+plot_tSNE <- function(data_file_path, channel=NULL, transform=NULL) {}
+
+
+# Identify number if cell clusters, not 100% sure how many is optimal, 
+# Detect number of clusters with NBclust?
+# it appears that overclustring is important
 # Clustering should be proportional to the cell number as you dont want loads of nodes with no cells in it 
-Clusters <- 100
 
-# Name of output directory  
-output_dir <- file.path("942_TAM_ALLPARAMS")
+RUN_SPADE_tSNE <- function(data_file_path, clusters=100, n=3, transforms='flowCore::arcsinhTransform(a=0, b=0.01))') {
+  Clusters <- 100
 
-# Select the markers to run the clustering on, this is going to be the tSNE channels
-markers <- c('APC-A', 'PE-A', 'Pacific Blue-A', 'AmCyan-A', 'APC-Cy7-A')
+  # Name of output directory
+  out <- strsplit(data_file_path,' ')
+  out_path <- out[[1]][1]
+  output_dir <- file.path(out_path)
 
-# not sure how to tramsform the other data, i don't think it really matters
-transforms <- c('APC-A'=flowCore::arcsinhTransform(a=0, b=0.01), 'AmCyan-A'=flowCore::arcsinhTransform(a=0, b=0.01), 'PE-A'=flowCore::arcsinhTransform(a=0, b=0.01), 'Pacific Blue-A'=flowCore::arcsinhTransform(a=0, b=0.01), 'APC-Cy7-A'=flowCore::arcsinhTransform(a=0, b=0.01))
+  # Creat master directory to put repeats in
+  
+  # Select SPADE to run on the tSNE channels the
+  # Note that the tSNE settings are embedded into channel name and must be the same  
+  markers <- c('tSNE_X_P_30_E_200_I_1000_T_0.2', 'tSNE_Y_P_30_E_200_I_1000_T_0.2')
 
-#Execute core SPADE algorithm
-SPADE.driver(data_file_path, out_dir=output_dir, transforms=transforms, cluster_cols=markers,k= Clusters)
+  # This is for specific marker transforms
+  # transforms <- c('APC-A'=flowCore::arcsinhTransform(a=0, b=0.01), 'AmCyan-A'=flowCore::arcsinhTransform(a=0, b=0.01), 'PE-A'=flowCore::arcsinhTransform(a=0, b=0.01), 'Pacific Blue-A'=flowCore::arcsinhTransform(a=0, b=0.01), 'APC-Cy7-A'=flowCore::arcsinhTransform(a=0, b=0.01))
 
-# Look at the files generated
-grep("^libloc",dir(output_dir),invert=TRUE,value=TRUE)
+  # Execute core SPADE algorithm in replicates of 3
+  # To generate 3 cluster tables to average and then heatmap
+  
+  for(i in 1:n){
+    
+    output_dir_n <- paste(output_dir, i, sep = '_') 
+    
+    # execute core SPADE command
+    spade::SPADE.driver(data_file_path, out_dir=output_dir_n, transforms=transforms, cluster_cols=markers,k= Clusters)
 
-# loade the graph object into memory
-mst_graph <- igraph:::read.graph(paste(output_dir,"mst.gml",sep=.Platform$file.sep),format="gml")
+    # load the graph object into memory
+    mst_graph <- igraph:::read.graph(paste(output_dir_n,"mst.gml",sep=.Platform$file.sep),format="gml")
 
-# Load the node marker intesity values for heatmapping 
-layout <- read.table(paste(output_dir,"layout.table",sep=.Platform$file.sep))
+    # Load the node marker intesity values for heatmapping 
+    layout <- read.table(paste(output_dir_n,"layout.table",sep=.Platform$file.sep))
 
-# Plot all the trees!! (In kamada kawai force driected graph mapping) 
-SPADE.plot.trees(mst_graph, output_dir, out_dir=paste(output_dir,"pdf",sep=.Platform$file.sep), layout=as.matrix(layout))
+    # Plot all the trees!! (In kamada kawai force driected graph mapping) 
+    SPADE.plot.trees(mst_graph, output_dir_n, out_dir=paste(output_dir_n,"pdf",sep=.Platform$file.sep), layout=as.matrix(layout))
 
-# Heatmap the sample table
+  }
+}
 
-# Import the node median fluoresencs values
-table_file_path <- paste(data_file_path, '.density.fcs.cluster.fcs.anno.Rsave_table.csv', sep='')
-table_path <- file.path(output_dir,'tables', 'bySample', table_file_path)
-table_path <- normalizePath(table_path)
-SPADE_nodes <- as.matrix(read.csv(table_path))
+# Include markers as vector to determine cluster markers
+# This is currently set up on TAM panel
+av_table_heatmap_SPADE <- function(data_file_path, n=3) {
 
-# Prune the table values so just the ones we want
-# Can automate this
-# removed count for now
-cluster_panel <- c('ID', 'mediansAPC.A_clust', 'mediansPE.A_clust', 'mediansPacific.Blue.A_clust', 'mediansAmCyan.A_clust', 'mediansAPC.Cy7.A_clust')
-prunedByChannel_SPADE_nodes <- SPADE_nodes[,cluster_panel]
+  # Function to average the three tables
+  # Then write out the average table and heatmap it
 
-# prune out minimum channels, do this later
-# pruned_SPADE_nodes <- prunedByChannel_SPADE_nodes[which(prunedByChannel_SPADE_nodes[,'count'] > 5)]
+  out <- strsplit(data_file_path,' ')
+  out_path <- out[[1]][1]
+  output_dir <- file.path(out_path)  
 
-# prune the table IDs into rownames
-# This is stupid as its dependant on the number of markers used
-x <- length(cluster_panel)
-rownames(prunedByChannel_SPADE_nodes) <- prunedByChannel_SPADE_nodes[,1]
-prunedByChannel_SPADE_nodes <- prunedByChannel_SPADE_nodes[,2:x]
+  for(p in 1:n) {
+  
+    output_dir_n <- paste(output_dir, p, sep = '_')  
 
-# arcsinh width basis is fidly, 0.01 seems to work quite well
-# If the data is previously transformed by SPADE no need
-# arcsinh_NODES <- asinh(prunedByChannel_SPADE_nodes*0.01) 
-arcsinh_NODES <- prunedByChannel_SPADE_nodes
+    # Import the node median fluoresencs values and add them together
+    table_file_path <- paste(data_file_path, '.density.fcs.cluster.fcs.anno.Rsave_table.csv', sep='')
+    table_path <- file.path(output_dir_n,'tables', 'bySample', table_file_path)
+    table_path <- normalizePath(table_path)
+    SPADE_nodes <- as.matrix(read.csv(table_path))
+  
+  if(p == 1) {
+    
+    combined_SPADE_nodes <- SPADE_nodes
+    print(SPADE_nodes[5,9])
+  }
+  
+  if(p > 1) {
+  
+    combined_SPADE_nodes <- combined_SPADE_nodes + SPADE_nodes
+    print(SPADE_nodes[5,9])  
+  }
+  }
 
-#Rename the columns
-colnames(arcsinh_NODES) = c('CD206', 'MHCII', 'F4/80', 'CD11b', 'CD11c')
+  #average the n combined tables  
+  av_SPADE_nodes <- combined_SPADE_nodes / n
+  print(n)
+  print(combined_SPADE_nodes[5,9])
+  print(av_SPADE_nodes[5,9])
+  
+  # Prune the table values so just the ones we want
+  # Use markers from input
+  
+  cluster_panel <- c('ID', 'mediansX.APC.A', 'mediansX.PE.A', 'mediansX.Pacific.Blue.A', 'mediansX.FITC.A','mediansX.APC.Cy7.A', 'mediansX.AmCyan.A')
+  prunedByChannel_SPADE_nodes <- av_SPADE_nodes[,cluster_panel]
+  
+  # write pruned by channel nodes to excel
+  write.csv(prunedByChannel_SPADE_nodes, file = 'av_spade_nodes.csv', sep = ",")
+  
+  # prune out minimum clusters at 1%, do this later
+  # pruned_SPADE_nodes <- prunedByChannel_SPADE_nodes[which(prunedByChannel_SPADE_nodes[,'count'] > 5)]
 
-# Do the heatmapping
-#Default heat scale- black, yellow, blue-- may need to change range depending on data values
-heat_palette <- colorRampPalette(c("black", "yellow", "#FAF7C9"))
-hot_pal <- heat_palette(80)
-bluecols <- brewer.pal(80, 'Blues')
-blue_pallette <- colorRampPalette(bluecols)
-ncols <- 81
-bluecols2 <- newcol(ncols)
+  # prune the table IDs into rownames
+  # This is stupid as its dependant on the number of markers used
+  x <- length(cluster_panel)
+  rownames(prunedByChannel_SPADE_nodes) <- prunedByChannel_SPADE_nodes[,1]
+  prunedByChannel_SPADE_nodes <- prunedByChannel_SPADE_nodes[,2:x]
 
-#heat_palette break for ramp
-heat_pairs.breaks <- seq(-1, 7, by=0.1)
+  # arcsinh width basis is fidly, 0.01 seems to work quite well
+  # If the data is previously transformed by SPADE no need
+  arcsinh_NODES <- asinh(prunedByChannel_SPADE_nodes*0.007) 
+  # arcsinh_NODES <- prunedByChannel_SPADE_nodes
+  
+  # Shift to row z-score ot bet represent the relative protein expression
+  
+  scale_nodes <- scale(prunedByChannel_SPADE_nodes) 
 
-# Blues breaks - note for pheatmap breaks must be a seq covering the range of values in x 1 element longer than colour vector.
-blue_break <- seq(0, 4,by=0.5)
+  #Rename the columns - this should be done with a double vector of markers and stains
+  colnames(arcsinh_NODES) = c('CD206', 'MHCII', 'F4_80', 'Ly6C', 'CD45', 'CD11b')
+  
+  
+  
+  # print(prunedByChannel_SPADE_nodes)
+  
+  # print(arcsinh_NODES) 
+  
+  # print(scale_nodes)
+  
+  # Do the heatmapping
+  
+  #Default heat scale- black, yellow, blue-- may need to change range depending on data values
+  heat_palette <- colorRampPalette(c("black", "yellow", "#FAF7C9"))
+  hot_pal <- heat_palette(80)
 
-# Need to fix messed up formatting of dendrogram
+  # heat_palette break for ramp
+  # heat_pairs.breaks <- seq(-1, 7, by=0.1)
 
-# dev.new(width=10, height=20)
-pdf('pheatmap_ALLPARAMS.pdf', paper='a4', width = 12, height = 15)
+  # Red blue heatmap
+  
+  print(max(arcsinh_NODES))
+  print(min(arcsinh_NODES))
+  
+  cols <- colorRampPalette(brewer.pal(10, "RdBu"))(256)
+  blue_breaks <- seq(min(prunedByChannel_SPADE_nodes), max(prunedByChannel_SPADE_nodes), by = (max(prunedByChannel_SPADE_nodes) - min(prunedByChannel_SPADE_nodes)) / 256) 
+  
+  # Need to fix messed up formatting of dendrogram 
+  # shifting to superheat package
+  
+  pdf_outfile <- paste(output_dir, '.pdf', sep = '')
+  
+  pdf(pdf_outfile, paper='a4', width = 12, height = 15)
 
-# Heatmap.2 execution
-# heatmap.2(arcsinh_NODES, main="SPADE node medians",breaks=scaled_pairs.breaks, dendrogram="both",trace="none",Rowv = TRUE, Colv=TRUE, revC=FALSE,symm=FALSE, symkey=FALSE,symbreaks=FALSE,scale="none",cexRow=0.6,cexCol=1,key=TRUE,col=heat_palette, density.info="none", margins = c(8,3), lmat = matrix(c(4,2,3,1), nrow=2, ncol=2) , lhei = c(0.1*2,0.9*2), lwid = c(0.3*(1/50),0.7*(1/50)))
+  # For pheatmap, calculate the optimal fontsize to fit the row
+  fontsize_row = 12 - nrow(arcsinh_NODES) / 15
 
-# For pheatmap, calculate the optimal fontsize to fit the row
-fontsize_row = 12 - nrow(arcsinh_NODES) / 15
+  # pheatmap execution
+  pheatmap(arcsinh_NODES, color = hot_pal,  scale = 'none', cellwidth = 9, cellheight =  7, fontsize_row = fontsize_row)
 
-# pheatmap execution
-pheatmap(arcsinh_NODES, color = hot_pal, breaks = heat_pairs.breaks,  scale = 'none', cellwidth = 9, cellheight =  7, fontsize_row = fontsize_row)
-
-dev.off()
+  # superheat execution
+  
+  dev.off()
+  
+}
